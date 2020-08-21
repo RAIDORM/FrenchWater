dbc3dad008b509b90ab61324c9b4d3301583c5b2
Thomas Houtrique <30757539+RAIDORM@users.noreply.github.com>
2020-08-22 00:35:34 +0200

    2.0.0 auto values get + easier code

--- README.md
+++ README.md
@@ -12,17 +12,31 @@ pip install frenchwater
 
 ## Get your informations
 
-![Test](test.gif)
-
+|  parameter  |    type    |                                link                                 |
+| :---------: | :--------: | :-----------------------------------------------------------------: |
+|   region    | str or int | https://fr.wikipedia.org/wiki/Codes_g%C3%A9ographiques_de_la_France |
+| departement |    str     | https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do  |
+|   commune   |    str     | https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do  |
+|   reseau    |    str     | https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do  |
 
 ## Usage
 
 ```python
 from FrenchWater import FrenchWater
 
-water = FrenchWater(region='93',departement='004',commune='04001',reseau='004001156_004')
+water = FrenchWater(region="93", departement="ALPES-DE-HAUTE-PROVENCE", commune="AIGLUN", reseau="AIGLUN VILLAGE")
+```
+
+```python
 water.get_last_results()
-water.get_last_x_results(result_size=20)
+# return the last results, it looks like this :
+{'Entérocoques /100ml-MS': ['<1 n/(100mL)', '14/04/2020'], 'Bact. et spores sulfito-rédu./100ml': ['<1 n/(100mL)', '14/04/2020'], ...}
+```
+
+```python
+water.get_last_x_results()
+# Return latest result for every key, and it looks like this:
+{'Entérocoques /100ml-MS': ['<1 n/(100mL)', '14/04/2020'], 'Bact. et spores sulfito-rédu./100ml': ['<1 n/(100mL)', '14/04/2020'], ...}
 ```
 
 ## Contributing
--- frenchwater.py
+++ frenchwater.py
@@ -3,31 +3,110 @@ from bs4 import BeautifulSoup
 
 
 class FrenchWater:
-    def __init__(self, region, departement, commune, reseau):
-        self.region = region
-        self.departement = departement
-        self.commune = commune
-        self.reseau = reseau
+    def __init__(self, region: str, departement: str, commune: str, reseau: str):
+        # define self parameters
 
-        self.session = requests.Session()
-        self.session.get(
-            "https://orobnat.sante.gouv.fr/orobnat/afficherPage.do?methode=menu&usd=AEP&idRegion=93"
-        )
-        self.cookie = self.session.cookies.get_dict()
+        self.region = str(region)
+        self.departement = str(departement)
+        self.commune = str(commune)
+        self.reseau = str(reseau)
+        self.url = "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do"
 
+        self.__get_cookie()
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
             "Content-Type": "application/x-www-form-urlencoded",
             "DNT": "1",
+            "Referer": "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do",
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
             "TE": "Trailers",
+            "Cookie": "JSESSIONID="
+            + self.cookie["JSESSIONID"]
+            + ";BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a="
+            + self.cookie["BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a"],
         }
+        self.__get_departement_id()
+        self.__get_commune_id()
+        self.__get_reseau_id()
 
-    def get_last_results(self):
-        url = "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do"
+    def __get_departement_id(self):
+        get_reseau_payload = "methode=changerDepartement&idRegion=" + self.region
+
+        response = requests.request(
+            "POST", self.url, headers=self.headers, data=get_reseau_payload
+        )
+        soup = BeautifulSoup(response.content, "lxml")
+        soupSelect = soup.select(
+            ".block-content > form:nth-child(2) > p:nth-child(6) > select:nth-child(2) > option"
+        )
+        if not soupSelect:
+            raise Exception(
+                "Departement not found please make sure your region id is correct")
+        for i in soupSelect:
+            if self.departement == i.text:
+                self.departement = i["value"]
+        return
+
+    def __get_commune_id(self):
+        get_reseau_payload = (
+            "methode=changerCommune&idRegion="
+            + self.region
+            + "&usd=AEP&posPLV=0&departement="
+            + self.departement
+        )
+
+        response = requests.request(
+            "POST", self.url, headers=self.headers, data=get_reseau_payload
+        )
+        soup = BeautifulSoup(response.content, "lxml")
+        soupSelect = soup.select(
+            ".block-content > form:nth-child(2) > p:nth-child(7) > select:nth-child(2) > option"
+        )
+        if not soupSelect:
+            raise Exception(
+                "commune not found please make sure your departement name is correct and your departement name is correct")
+        for i in soupSelect:
+            if self.commune == i.text:
+                self.commune = i["value"]
+        return
+
+    def __get_reseau_id(self):
+        get_reseau_payload = (
+            "methode=changerReseau&idRegion="
+            + self.region
+            + "&usd=AEP&posPLV=0&departement="
+            + self.departement
+            + "&communeDepartement="
+            + self.commune
+        )
+
+        response = requests.request(
+            "POST", self.url, headers=self.headers, data=get_reseau_payload
+        )
+        soup = BeautifulSoup(response.content, "lxml")
+        soupSelect = soup.select(
+            ".block-content > form:nth-child(2) > p:nth-child(8) > select:nth-child(2) > option"
+        )
+        if not soupSelect:
+            raise Exception(
+                "reseau not found please make sure your commune name is correct and your reseau name is correct")
+        for i in soupSelect:
+            if self.reseau == i.text:
+                self.reseau = i["value"]
+        return
+
+    def __get_cookie(self):
+        self.session = requests.Session()
+        self.session.get(
+            "https://orobnat.sante.gouv.fr/orobnat/afficherPage.do?methode=menu&usd=AEP&idRegion=93"
+        )
+        self.cookie = self.session.cookies.get_dict()
+        return
+
+    def get_last_results(self) -> (dict):
 
         payload = (
             "methode=rechercher&idRegion="
@@ -40,19 +119,8 @@ class FrenchWater:
             + self.reseau
         )
 
-        water_headers = self.headers
-        water_headers[
-            "Referer"
-        ] = "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do"
-        water_headers["Cookie"] = (
-            "JSESSIONID="
-            + self.cookie["JSESSIONID"]
-            + ";BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a="
-            + self.cookie["BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a"]
-        )
-
         response = self.session.request(
-            "POST", url, headers=water_headers, data=payload
+            "POST", url, headers=self.headers, data=payload
         )
         if response.status_code != 200:
             raise ConnectionError(f"Error code {response.status_code}")
@@ -69,7 +137,7 @@ class FrenchWater:
                     dic[key.text] = [value.text, date]
         return dic
 
-    def get_last_x_results(self, result_size):
+    def get_last_x_results(self, result_size: int) -> (dict):
         dic = {}
         for i in range(0, result_size):
             url = "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do"
@@ -86,19 +154,9 @@ class FrenchWater:
                 + "&reseau="
                 + self.reseau
             )
-            water_headers = self.headers
-            water_headers[
-                "Referer"
-            ] = "https://orobnat.sante.gouv.fr/orobnat/rechercherResultatQualite.do"
-            water_headers["Cookie"] = (
-                "JSESSIONID="
-                + self.cookie["JSESSIONID"]
-                + ";BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a="
-                + self.cookie["BWFSESSID_vk35skh3d745n9jzlgg5lvcl2a"]
-            )
 
             response = self.session.request(
-                "POST", url, headers=water_headers, data=payload
+                "POST", url, headers=self.headers, data=payload
             )
             if response.status_code != 200:
                 raise ConnectionError(f"Error code {response.status_code}")
